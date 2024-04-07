2c17094cf0328bf78656cd5d9cfac57f50dd7a42
Viviane <vivialves.v@gmail.com>
2024-04-07 01:52:19 -0400

    update menu

--- menu.py
+++ menu.py
@@ -1,4 +1,3 @@
-from time import sleep
 from google.oauth2 import service_account
 
 from basisclasses.secure_file_transfer import SecureFileTransfer
@@ -33,6 +32,9 @@ def main() -> None:
                      uploaded_file.getvalue(),
                      )
         plain_text = SecureFileTransfer(plain_text=uploaded_file.getvalue())
+        doc_ref.update({
+            'plain_text': uploaded_file.getvalue()
+        })
         st.divider()
         col1, col2 = st.columns(2)
         with col1:
@@ -41,22 +43,24 @@ def main() -> None:
                 serialize_rsa_private_key = plain_text.serialize_rsa_private_key()
                 st.text_area('', serialize_rsa_private_key)
                 st.write(':orange[Private key generated successful!]')
-                doc_ref.set({
+                doc_ref.update({
                     'private_key': serialize_rsa_private_key
                 })
-                st.write(':blue[Would you like to download?]')
-                # st.download_button('Download', doc.get('private_key'))
+                # st.write(':blue[Would you like to download?]')
+                # if st.download_button('Download', doc.get('private_key'), file_name='private key'):
+                    # st.write(':blue[Download]')
         with col2:
             button2 = st.button('Generated Public Key')
             if button2:
                 serialize_rsa_public_key = plain_text.serialize_rsa_public_key()
                 st.text_area('', serialize_rsa_public_key)
                 st.write(':orange[Public key generated successful!]')
-                st.write(':blue[Would you like to download?]')
                 doc_ref.update({
                     'public_key': serialize_rsa_public_key
                 })
-                # st.download_button('Download', doc.get("public_key"))
+                # st.write(':blue[Would you like to download?]')
+                # if st.download_button('Download', doc.get("public_key"), file_name='public key'):
+                    # st.write(':blue[Download]')
         st.divider()
         st.subheader("Would you like to encrypt or generating a hash?")
         col3, col4 = st.columns(2)
@@ -66,23 +70,27 @@ def main() -> None:
                 encrypted = plain_text.encryption(serialized_public_key=doc.get('public_key'))
                 st.text_area('', encrypted)
                 st.write(':orange[Message encrypted successful!]')
-                st.write(':blue[Would you like to download?]')
+
                 doc_ref.update({
                     "encrypted": encrypted
                 })
-                # st.download_button('Download', doc.get('encrypted'))
+                # st.write(':blue[Would you like to download?]')
+                # if st.download_button('Download', doc.get('encrypted'), file_name=encrypted):
+                    # st.write(':blue[Download]')
         with col4:
             button4 = st.button('Hashing')
             if button4:
                 hashing = plain_text.hashing()
                 st.text_area('', hashing)
                 st.write(':orange[Hashing generated successful!]')
-                st.write(':blue[Would you like to download?]')
                 doc_ref.update({
                     "hashing": hashing
                 })
-                # st.download_button('Download', doc.get('hashing'))
+                # st.write(':blue[Would you like to download?]')
+                # if st.download_button('Download', doc.get('hashing')):
+                    # st.write(':blue[Download]')
         st.divider()
+        st.subheader("Would you like to decrypt or check integrity?")
         col5, col6 = st.columns(2)
         with col5:
             button5 = st.button('Decryption')
@@ -92,11 +100,13 @@ def main() -> None:
                                                    )
                 st.text_area('', decryption)
                 st.write(':orange[Message decrypted successful]')
-                st.write(':blue[Would you like to download?]')
+
                 doc_ref.update({
                     "decrypted_message": decryption
                 })
-                # st.download_button('Download', doc.get('decrypted_message'))
+                # st.write(':blue[Would you like to download?]')
+                # if st.download_button('Download', doc.get('decrypted_message')):
+                    # st.write(':blue[Download]')
         with col6:
             button6 = st.button('Integrity verification')
             if button6:
